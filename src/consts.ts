c0b5c9ab4efbd59afea9e09543806b9a1cefa0fb
Kevin <vincitbussiness@gmail.com>
2024-10-22 23:10:22 -0600

    Cambiando titulo y descripcion

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Un Joven Adventista - Devocionales y juegos bíblicos para jóvenes';
+export const SITE_DESCRIPTION = 'Encuentra devocionales, ejercicios bíblicos, programas de sociedad de jóvenes y todo lo que un jóven adventista pueda necesitar';
